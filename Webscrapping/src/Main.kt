aef6cf205280fd6dedc1ca199267b5e8d7661dbf
Michaël Van Meerbeek <michael.van.meerbeek@student.ehb.be>
2017-11-17 11:46:24 +0100

    Products can be transformed to json

--- Webscrapping/src/Main.kt
+++ Webscrapping/src/Main.kt
@@ -1,7 +1,13 @@
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.mda.nubel.model.Product
 import com.mda.nubel.scraper.ProductScraper
 
 fun main(args: Array<String>) {
     val scraper = ProductScraper(com.mda.nubel.scraper.sessionId)
 
-    print(scraper.scrapProductGroups())
+    val mapper = jacksonObjectMapper()
+    val writer = mapper.writerWithDefaultPrettyPrinter()
+
+    val json = writer.writeValueAsString(scraper.scrapProduct(4498))
+    print(json)
 }
